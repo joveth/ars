548cce1fe26ec17a00968c04f4a7d8d13078505a
qiaowei <jov123@163.com>
2015-08-14 10:38:53 +0800

    all

---
+++ Ars/MoreDetailController.h
@@ -0,0 +1,13 @@
+//
+//  MoreDetailController.h
+//  Ars
+//
+//  Created by Shuwei on 15/8/13.
+//  Copyright (c) 2015年 jov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreDetailController : UIViewController
+
+@end
